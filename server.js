c15c6f2f5c7335d8a1b365ccb040ca0a92c0a561
Rakesh Shenoy <m.rakesh.shenoy@gmail.com>
2017-04-01 04:22:45 -0700

    On Azure Test

--- server.js
+++ server.js
@@ -18,9 +18,6 @@ io.on('connection', function(socket){
     console.log('user disconnected');
   });
 });
-  
 
-app.listen(process.env.port, function(){
-  console.log('listening on *:3000');
-});
+app.listen(process.env.PORT || 3000);
     
