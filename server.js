98092cf41d38c10cbb5578e54d49ec1dd54029f0
Rakesh Shenoy <m.rakesh.shenoy@gmail.com>
2017-04-01 05:00:51 -0700

    Hello World

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.render('index.html');
+  res.sendFile('index.html');
   //res.writeHead(200, { 'Content-Type': 'text/plain' });
   //res.end('Hello World\n');
 });
